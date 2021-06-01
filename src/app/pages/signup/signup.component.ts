9cc4e4035041e3c3bbcce12c31461fb71d5f35d6
kaushikyelne <kaushik.s.yelne@gmail.com>
2021-06-01 18:51:02 +0530

    Showing classic Messages using MatSnackBar and SweetAlert while Registration

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from 'src/app/user.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-signup',
@@ -8,7 +10,7 @@ import { UserService } from 'src/app/user.service';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private userService:UserService) { }
+  constructor(private userService:UserService, private snack:MatSnackBar) { }
 
   public user ={
       username: '',
@@ -25,22 +27,31 @@ export class SignupComponent implements OnInit {
   formSubmit(){
     console.log(this.user);
     if(this.user.username =='' || this.user.username == null){
-      alert('submit');
+      // alert('submit');
+      this.snack.open('Username is required !!','',{
+        duration:3000,
+        // verticalPosition: 'top',
+        // horizontalPosition: 'center',
+      });
       return;
     }
 
+    //addUser: userservice
     this.userService.addUser(this.user).subscribe(
       (data)=>{
         //success
         console.log(data);
-        alert('success')
+        // alert('success')
+        Swal.fire('Success','user is registered','success');
         
       },
       (error)=>{
         //error
         console.log(error);
-        alert('something went wrong')
-        
+        // alert('something went wrong')
+        this.snack.open('something went wrong !!','',{
+          duration:3000,
+        })
       }
     );
   }
