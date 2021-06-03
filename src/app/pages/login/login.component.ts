17512f02c496edcedab56805893d80880924e95a
kaushikyelne <kaushik.s.yelne@gmail.com>
2021-06-03 15:37:32 +0530

    Login at frontend

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {
+    username: '',
+    password: '',
+  }
+
+  constructor(private snak:MatSnackBar,private login:LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  formSubmit(){
+    console.log("login working");
+
+    if(this.loginData.username.trim()=='' || this.loginData.username==null){
+
+      this.snak.open("Username is required",'',{
+        duration:3000,
+      });
+      return;
+
+    }
+    if(this.loginData.password.trim()=='' || this.loginData.password==null){
+
+      this.snak.open("Password is required",'',{
+        duration:3000,
+      });
+      return;
+
+    }
+
+    // request to server to generate token
+    this.login.generteToken(this.loginData).subscribe(
+      (data:any)=>{
+        console.log("success");
+        console.log(data);
+        
+        
+      },
+      (error)=>{
+        console.log('Error');
+        console.log(error);
+        
+      }
+    );
+
   }
 
 }
